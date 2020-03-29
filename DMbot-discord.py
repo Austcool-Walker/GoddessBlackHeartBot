4a97e41bee42d77bae90ede765d5067a50040c03
Austin Walker <noire@Austins-iMac.local>
2020-03-29 17:33:43 -0400

    updated with a few new features

--- DMbot-discord.py
+++ DMbot-discord.py
@@ -1,7 +1,25 @@
 #															Send DMs to people using bots (Python)
-import asyncio
+import discord
+from discord.ext.commands.cooldowns import BucketType
 from discord.ext.commands import Bot, Greedy
+from discord.ext.tasks import loop
 from discord import User
+from asyncio import sleep
+import time
+import random
+import json
+import sys
+import asyncio
+import inspect
+import json
+import os
+import asyncio
+import asyncpg
+from datetime import datetime
+import random
+import logging
+import aiohttp
+import traceback
 
 # This is prefix of my bot
 bot = Bot(command_prefix='!')
@@ -13,7 +31,66 @@ async def pm(ctx, users: Greedy[User], *, message):
         await user.send(message)
 
 
-# Finally add your token number and run the client
-bot.run("Discord Auth Token Here!")
+# About embed
+@bot.command()
+async def about(ctx):
+    embed = discord.Embed(title="DMbot-discord", description="A bot for sending Discord DMs to users.", color=0xff00e6)
 
+    # give info about you here
+    embed.add_field(name="Author", value="<@318528448320634881>")
+
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+    # Source Code URL:
+    embed.add_field(name="Source Code", value="https://github.com/Austcool-Walker/DM-discord-bot.git")
+
+    # Your personal Discord Server that the bot was made for.
+    embed.add_field(name="Discord Server", value="https://discord.gg/veVDS47")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="[Invite link](https://discordapp.com/api/oauth2/authorize?client_id=693568262813909072&permissions=8&scope=bot)")
+
+    await ctx.send(embed=embed)
+
+# Bots Status
+# Setting `Playing ` status
+# await bot.change_presence(activity=discord.Game(name="The Overcomplicated Weirdness 1.12.2"))
+
+# Setting `Streaming ` status
+# await bot.change_presence(activity=discord.Streaming(name="Approaching Nirvana", url="https://www.twitch.tv/approachingnirvana"))
+
+# Setting `Listening ` status
+# await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Reboot by Approaching Nirvana & Big Giant Circles"))
+
+# Setting `Watching ` status
+# await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Azur Lane"))
+
+# Eval command
+@bot.command(name='eval', pass_context=True)
+async def eval_(ctx, *, command):
+    res = eval(command)
+    if inspect.isawaitable(res):
+        await bot.say(await res)
+    else:
+        await bot.say(res)
+
+
+@bot.command()
+async def bowtourqueen(ctx):
+    await ctx.send("https://i.imgur.com/t2LE30K.png")
+
+bot.remove_command('help')
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="DMbot-discord", description="A bot for sending Discord DMs to users. List of commands are:", color=0xff00e6)
+    embed.add_field(name="!about", value="Gives information about this bot.", inline=False)
+    embed.add_field(name="!pm", value="Sends user a message in a DM. The bot must share a server with the user you wish to send the message to.", inline=False)
+    embed.add_field(name="!bowtourqueen", value="Sends a image of Goddess Black Heart from neptunia (meme)", inline=False)
+    embed.add_field(name="!help", value="Gives this help message", inline=False)
+    await ctx.send(embed=embed)
+
+# Finally add your token number and run the client
+bot.run("NjkzNTY4MjYyODEzOTA5MDcy.Xn_ZAw._oXUNmdat_gkL0e_wy4yOQ4JJ9s")
 
