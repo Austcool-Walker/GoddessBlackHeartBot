c64cd14f8076e602b1336fcfa2db331318bd4450
Austcool-Walker <0ajwalker@gmail.com>
2020-03-30 11:52:12 -0400

    updated the about function

--- DMbot-discord.py
+++ DMbot-discord.py
@@ -46,7 +46,8 @@ async def about(ctx):
     embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
 
     # Source Code URL:
-    sc = embed.set_image(url="https://github.com/Austcool-Walker/DM-discord-bot.git")
+    sc = 'https://github.com/Austcool-Walker/DM-discord-bot.git'
+    embed.set_image(url=[sc])
     embed.add_field(name="Source Code", value=[sc])
     
 
