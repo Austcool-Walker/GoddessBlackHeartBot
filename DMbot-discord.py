7dd232ccba607bd6f56fa0b06be83f80308208ab
Austcool-Walker <0ajwalker@gmail.com>
2020-03-30 13:59:07 -0400

    added bot status change ability

--- DMbot-discord.py
+++ DMbot-discord.py
@@ -131,7 +131,7 @@ async def eval_(ctx, *, command):
 # Set bot's status
 
 count = requests.get(file=".botstatus")
-await client.change_presence(game=discord.Game(name=count.text, type=3))
+await bot.change_presence(game=discord.Game(name=count.text, type=3))
 
 # Finally add your token number and run the client
 bot.run("Discord Auth Token Here!")
