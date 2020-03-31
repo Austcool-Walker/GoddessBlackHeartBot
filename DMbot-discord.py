71668bb5f87efe68484d8f1f7148d258eb3098f1
Austcool-Walker <0ajwalker@gmail.com>
2020-03-31 18:40:48 -0400

    Added moderation commands & Adminstrative commands

--- DMbot-discord.py
+++ DMbot-discord.py
@@ -158,6 +158,75 @@ async def say(ctx, *, message: str):
     await ctx.message.delete()
     await ctx.send(message)
 
+# Moderation commands
+@bot.command()
+async def kick(ctx, user: discord.User, *, reason: str):
+    if not ctx.message.channel.permissions_for(ctx.message.author.guild.me).kick_members:
+        await ctx.send(":x: I do not have permission to kick players.")
+        return
+    try:
+        await ctx.message.guild.kick(user, reason=reason)
+    except Exception:
+        await ctx.send(":x: Player kick failed.")
+        return
+    await ctx.send(":white_check_mark: Player {} has been kicked from the server.".format(user.name))
+
+@bot.command()
+async def ban(ctx, user: discord.User, *, reason: str):
+    if not ctx.message.channel.permissions_for(ctx.message.author.guild.me).ban_members:
+        await ctx.send(":x: I do not have permission to ban players.")
+        return
+    try:
+        await ctx.message.guild.ban(user, reason=reason)
+    except Exception:
+        await ctx.send(":x: I completely failed to ban that player.")
+        return
+    await ctx.send(":white_check_mark: Player {} has been banned from the server.".format(user.name))
+
+@bot.command()
+async def mute(ctx, user: discord.Member):
+    if not ctx.message.channel.permissions_for(ctx.message.author.guild.me).manage_roles:
+        await ctx.send(":x: I do not have permission to manage roles.")
+        return
+    try:
+        await ctx.message.channel.category.set_permissions(user, send_messages=False, add_reactions=False)
+    except Exception:
+        await ctx.send("I was unable to mute that player.")
+        return
+    await ctx.send(":white_check_mark: Player {} has been muted.".format(user.display_name))
+
+@bot.command()
+async def unmute(ctx, user: discord.Member):
+    if not ctx.message.channel.permissions_for(ctx.message.author.guild.me).manage_roles:
+        await ctx.send(":x: I do not have permission to manage roles.")
+        return
+    try:
+        await ctx.message.channel.category.set_permissions(user, overwrite=None)
+    except Exception:
+        await ctx.send("I was unable to unmute that player.")
+        return
+    await ctx.send(":white_check_mark: Player {} has been unmuted.".format(user.display_name))
+
+@bot.command()
+async def prune(ctx, number: int):
+    if not ctx.message.channel.permissions_for(ctx.message.author.guild.me).manage_messages:
+        await ctx.send("I do not have permission to delete messages.")
+        return
+    if number > 500:
+        await ctx.send("Please specify a lower number.")
+        return
+    to_delete = []
+    async for message in ctx.message.channel.history(limit=number+1):
+        to_delete.append(message)
+    while to_delete:
+        if len(to_delete) > 1:
+            await ctx.message.channel.delete_messages(to_delete[:100])
+            to_delete = to_delete[100:]
+        else:
+            await to_delete.delete()
+            to_delete = []
+        await asyncio.sleep(1.5)
+
 # Eval bot command
 
 def insert_returns(body):
@@ -228,6 +297,186 @@ async def eval_fn(ctx, *, cmd):
 
     result = (await eval(f"{fn_name}()", env))
     await ctx.message.add_reaction('👌')
+
+# Admin bot commands
+
+@bot.command(aliases=['quit'], hidden=True)
+async def shutdown(ctx):
+    '''Schaltet mich ab :( (BOT OWNER ONLY)'''
+    await ctx.send('**:ok:** Bye!')
+    #bot.gamesLoop.cancel()
+    await bot.logout()
+    sys.exit(0)
+
+@bot.command(hidden=True)
+async def restart(ctx):
+    '''Startet mich neu (BOT OWNER ONLY)'''
+    await ctx.send('**:ok:** Bis gleich!')
+    await bot.logout()
+    sys.exit(6)
+
+@bot.command(hidden=True)
+async def botavatar(ctx, url: str):
+    '''Setzt einen neuen Avatar (BOT OWNER ONLY)'''
+    tempAvaFile = 'tempAva.png'
+    async with aiohttp.get(''.join(url)) as img:
+        with open(tempAvaFile, 'wb') as f:
+            f.write(await img.read())
+    f = discord.File(tempAvaFile)
+    await bot.edit_profile(avatar=f.read())
+    os.remove(tempAvaFile)
+    asyncio.sleep(2)
+    await ctx.send('**:ok:** Mein neuer Avatar!\n %s' % bot.user.avatar_url)
+
+@bot.command(hidden=True, aliases=['game'])
+async def changegame(ctx, gameType: str, *, gameName: str):
+    '''?ndert das derzeit spielende Spiel (BOT OWNER ONLY)'''
+    gameType = gameType.lower()
+    if gameType == 'playing':
+        type = discord.Activity.playing
+    elif gameType == 'watching':
+        type = discord.Activity.watching
+    elif gameType == 'listening':
+        type = discord.Activity.listening
+    elif gameType == 'streaming':
+        type = discord.Activity.streaming
+    guildsCount = len(bot.guilds)
+    memberCount = len(list(bot.get_all_members()))
+    gameName = gameName.format(guilds = guildsCount, members = memberCount)
+    await bot.change_presence(activity=discord.Activity(type=type, name=gameName))
+    await ctx.send(f'**:ok:** ?ndere das Spiel zu: {gameType} **{gameName}**')
+
+@bot.command(hidden=True)
+async def changestatus(ctx, status: str):
+    '''?ndert den Online Status vom Bot (BOT OWNER ONLY)'''
+    status = status.lower()
+    if status == 'offline' or status == 'off' or status == 'invisible':
+        discordStatus = discord.Status.invisible
+    elif status == 'idle':
+        discordStatus = discord.Status.idle
+    elif status == 'dnd' or status == 'disturb':
+        discordStatus = discord.Status.dnd
+    else:
+        discordStatus = discord.Status.online
+    await bot.change_presence(status=discordStatus)
+    await ctx.send(f'**:ok:** ?ndere Status zu: **{discordStatus}**')
+
+@bot.command(hidden=True)
+async def name(ctx, name: str):
+    '''?ndert den globalen Namen vom Bot (BOT OWNER ONLY)'''
+    await bot.edit_profile(username=name)
+    msg = f':ok: ?ndere meinen Namen zu: **{name}**'
+    await ctx.send(msg)
+
+@bot.command(hidden=True, aliases=['guilds'])
+async def servers(ctx):
+    '''Listet die aktuellen verbundenen Guilds auf (BOT OWNER ONLY)'''
+    msg = '```js\n'
+    msg += '{!s:19s} | {!s:>5s} | {} | {}\n'.format('ID', 'Member', 'Name', 'Owner')
+    for guild in bot.guilds:
+        msg += '{!s:19s} | {!s:>5s}| {} | {}\n'.format(guild.id, guild.member_count, guild.name, guild.owner)
+    msg += '```'
+    await ctx.send(msg)
+
+@bot.command(hidden=True)
+async def leaveserver(ctx, guildid: str):
+    '''Tritt aus einem Server aus (BOT OWNER ONLY)
+
+    Beispiel:
+    -----------
+
+    :leaveserver 102817255661772800
+    '''
+    if guildid == 'this':
+        await ctx.guild.leave()
+        return
+    else:
+        guild = bot.get_guild(guildid)
+        if guild:
+            await guild.leave()
+            msg = f':ok: Austritt aus {guild.name} erfolgreich!'
+        else:
+            msg = ':x: Konnte keine passende Guild zu dieser ID finden!'
+    await ctx.send(msg)
+
+@bot.command(hidden=True)
+async def echo(ctx, channel: str, *message: str):
+    '''Gibt eine Nachricht als Bot auf einem bestimmten Channel aus (BOT OWNER ONLY)'''
+    ch = bot.get_channel(int(channel))
+    msg = ' '.join(message)
+    await ch.send(msg)
+    await ctx.message.delete()
+
+@bot.command(hidden=True)
+async def discriminator(ctx, disc: str):
+    '''Gibt Benutzer mit dem jeweiligen Discriminator zur?ck'''
+
+    discriminator = disc
+    memberList = ''
+
+    for guild in bot.guilds:
+        for member in guild.members:
+            if member.discriminator == discriminator and member.discriminator not in memberList:
+                memberList += f'{member}\n'
+
+    if memberList:
+        await ctx.send(memberList)
+    else:
+        await ctx.send(':x: Konnte niemanden finden')
+
+@bot.command(hidden=True)
+async def nickname(ctx, *name):
+    '''?ndert den Server Nickname vom Bot (BOT OWNER ONLY)'''
+    nickname = ' '.join(name)
+    await ctx.me.edit(nick=nickname)
+    if nickname:
+        msg = f':ok: ?ndere meinen Server Nickname zu: **{nickname}**'
+    else:
+        msg = f':ok: Reset von meinem Server Nickname auf: **{ctx.me.name}**'
+    await ctx.send(msg)
+
+@bot.command(hidden=True)
+async def setnickname(ctx, member: discord.Member=None, *name):
+    '''?ndert den Nickname eines Benutzer (BOT OWNER ONLY)'''
+    if member == None:
+        member = ctx.author
+    nickname = ' '.join(name)
+    await member.edit(nick=nickname)
+    if nickname:
+        msg = f':ok: ?ndere Nickname von {member} zu: **{nickname}**'
+    else:
+        msg = f':ok: Reset von Nickname f?r {member} auf: **{member.name}**'
+    await ctx.send(msg)
+
+@bot.command(hidden=True)
+async def geninvite(ctx, serverid: str):
+    '''Generiert einen Invite f?r eine Guild wenn m?glich (BOT OWNER ONLY)'''
+    guild = bot.get_guild(int(serverid))
+    invite = await bot.create_invite(guild, max_uses=1, unique=False)
+    msg = f'Invite f?r **{guild.name}** ({guild.id})\n{invite.url}'
+    await ctx.author.send(msg)
+
+@bot.command(hidden=True, aliases=['wichteln'])
+async def wichtel(ctx, *participants: str):
+    '''N?tzlich f?r das Community Wichtel Event 2018 (BOT OWNER ONLY)'''
+    participantsList = list(participants)
+    random.shuffle(participantsList)
+    msg = 'Wichtelpartner stehen fest:\n```'
+    for i, val in enumerate(participantsList):
+        if i == len(participantsList) - 1:
+            msg += f'{val.ljust(10)} ===> {participantsList[0]}\n'
+        else:
+            msg += f'{val.ljust(10)} ===> {participantsList[i + 1]}\n'
+
+    msg += '```'
+    await ctx.send(msg)
+
+@bot.command(hidden=True)
+async def test(ctx):
+    '''Test Test Test'''
+    await ctx.send('Test')
+    await bot.AppInfo.owner.send('Test')
+    await ctx.send(bot.cogs)
 
 # Finally add your token number and run the client
 bot.run("Discord Auth Token Here!")
