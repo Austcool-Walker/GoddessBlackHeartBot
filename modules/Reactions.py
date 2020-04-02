29ce0d76b750ae2654d5615d4a617b385d5602ff
Austcool-Walker <0ajwalker@gmail.com>
2020-04-02 19:32:11 -0400

    Fixed Reactions index. 2

--- modules/Reactions.py
+++ modules/Reactions.py
@@ -4,34 +4,34 @@ from discord.ext import commands
 
 class Reactions(commands.Cog, name="Reactions"):
 
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, Client):
+        self.Client = Client
 
-        @bot.event
+        @Client.event
         async def on_message(message):
                 channel = message.channel
                 if message.content.startswith('rage'):
                     await ctx.message.add_reaction('🤬')
 
-        @bot.event
+        @Client.event
         async def on_message(message):
                 channel = message.channel
                 if message.content.startswith('mad, angry'):
                     await ctx.message.add_reaction('😡')
 
-        @bot.event
+        @Client.event
         async def on_message(message):
                 channel = message.channel
                 if message.content.startswith('sad , despair'):
                     await ctx.message.add_reaction('😭')
 
-        @bot.event
+        @Client.event
         async def on_message(message):
                 channel = message.channel
                 if message.content.startswith('love'):
                     await ctx.message.add_reaction('❤️')
 
-        @bot.event
+        @Client.event
         async def on_message(message):
                 channel = message.channel
                 if message.content.startswith('happy, joy'):
