77999f6b0efd1f668ca045edd975081d6bde599e
Austcool-Walker <0ajwalker@gmail.com>
2020-04-02 19:21:45 -0400

    Fixed indentation errors. 4

--- modules/Reactions.py
+++ modules/Reactions.py
@@ -7,35 +7,35 @@ class Reactions(commands.Cog, name="Reactions"):
     def __init__(self, bot, Client):
         self.bot = bot
 
-   @bot.event
-    async def on_message(message):
-            channel = message.channel
-            if message.content.startswith('rage'):
-                await ctx.message.add_reaction('🤬')
+        @bot.event
+        async def on_message(message):
+                channel = message.channel
+                if message.content.startswith('rage'):
+                    await ctx.message.add_reaction('🤬')
 
-   @bot.event
-    async def on_message(message):
-            channel = message.channel
-            if message.content.startswith('mad', 'angry'):
-                await ctx.message.add_reaction('😡')
+        @bot.event
+        async def on_message(message):
+                channel = message.channel
+                if message.content.startswith('mad', 'angry'):
+                    await ctx.message.add_reaction('😡')
 
-   @bot.event
-    async def on_message(message):
-            channel = message.channel
-            if message.content.startswith('sad' , 'despair'):
-                await ctx.message.add_reaction('😭')
+        @bot.event
+        async def on_message(message):
+                channel = message.channel
+                if message.content.startswith('sad' , 'despair'):
+                    await ctx.message.add_reaction('😭')
 
-   @bot.event
-    async def on_message(message):
-            channel = message.channel
-            if message.content.startswith('love'):
-                await ctx.message.add_reaction('❤️')
+        @bot.event
+        async def on_message(message):
+                channel = message.channel
+                if message.content.startswith('love'):
+                    await ctx.message.add_reaction('❤️')
 
-   @bot.event
-    async def on_message(message):
-            channel = message.channel
-            if message.content.startswith('happy', 'joy'):
-                await ctx.message.add_reaction('😁')
+        @bot.event
+        async def on_message(message):
+                channel = message.channel
+                if message.content.startswith('happy', 'joy'):
+                    await ctx.message.add_reaction('😁')
 
 
 def setup(bot):
