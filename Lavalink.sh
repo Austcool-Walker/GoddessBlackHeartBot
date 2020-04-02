4c0f8087081bfd65343a91432f147f4d7687c1f7
Austcool-Walker <0ajwalker@gmail.com>
2020-04-02 18:08:27 -0400

    Updated Lavalink.sh

--- Lavalink.sh
+++ Lavalink.sh
@@ -1,2 +1,17 @@
 #!/bin/zsh
-macOS/Contents/Home/bin/java -jar -Xmx512M Lavalink.jar
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        Linux/Contents/Home/bin/java -jar -Xmx1G Lavalink.jar
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        macOS/Contents/Home/bin/java -jar -Xmx1G Lavalink.jar
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+        echo "Your OSTYPE could not be detected!"
+fi
