bcf6025de2e0a1f4bcbf76b3f3bb4e626953bfdc
Austcool-Walker <0ajwalker@gmail.com>
2020-04-02 18:48:17 -0400

    Added Reactions Feature.

---
+++ modules/Reactions.py
@@ -0,0 +1,42 @@
+#              Send DMs to people using bots (Python)
+import discord
+from discord.ext import commands
+
+class Reactions(commands.Cog, name="Reactions"):
+
+	def __init__(self, bot):
+		self.bot = bot
+    
+    @Client.event
+    async def on_message(message):
+        channel = message.channel
+        if message.content.startswith('.rage'):
+            await ctx.message.add_reaction('🤬')
+
+    @Client.event
+    async def on_message(message):
+        channel = message.channel
+        if message.content.startswith('.mad', '.angry'):
+            await ctx.message.add_reaction('😡')
+
+    @Client.event
+    async def on_message(message):
+        channel = message.channel
+        if message.content.startswith('.sad' , '.despair'):
+            await ctx.message.add_reaction('😭')
+
+    @Client.event
+    async def on_message(message):
+        channel = message.channel
+        if message.content.startswith('.love'):
+            await ctx.message.add_reaction('❤️')
+
+    @Client.event
+    async def on_message(message):
+        channel = message.channel
+        if message.content.startswith('.happy', '.joy'):
+            await ctx.message.add_reaction('😁')
+
+
+def setup(bot):
+	bot.add_cog(Reactions(bot))
