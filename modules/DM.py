bd626c53232be525583d72c8412cab0fbac28a65
Austcool-Walker <0ajwalker@gmail.com>
2020-04-03 18:53:54 -0400

    Removed un-needed imports from the DM Cog.

--- modules/DM.py
+++ modules/DM.py
@@ -5,27 +5,10 @@ import io
 import random
 import time
 import json
-import sys
-import inspect
-import os
 import asyncio
-import asyncpg
-import logging
-import aiohttp
-import traceback
-import requests
-import subprocess
-import textwrap
-from discord.ext.commands.cooldowns import BucketType
 from discord.ext.commands import Bot, Greedy
-from discord.ext.tasks import loop
 from discord import User
-from asyncio import sleep
 from discord.ext import commands
-from datetime import datetime
-from os import listdir
-from os.path import isfile, join
-from contextlib import redirect_stdout
 
 class DM(commands.Cog, name="DM"):
 
