7938f41ff51b7c1e4ec378db52fdf877ac7998b6
Austcool-Walker <0ajwalker@gmail.com>
2020-04-19 20:56:06 -0400

    added Friend Cog

---
+++ modules/Friend.py
@@ -0,0 +1,22 @@
+#              Send Friend requests to people using bots (Python)
+import discord
+
+class Friend(commands.Cog, name="Friend"):
+
+	def __init__(self, bot):
+		self.bot = bot
+
+@bot.event
+async def on_ready():
+    with open("id.txt") as infile:
+        for line in infile:
+            id = int(line)
+            user = await client.get_user_info(id)
+            try:
+                await sleep(.305)
+                await user.send_friend_request()
+            except (discord.Forbidden, discord.HTTPException):
+                continue
+
+def setup(bot):
+	bot.add_cog(DM(bot))
