0d0310f9c7469a472dcf21068efdb05eeb458cd4
Austcool-Walker <0ajwalker@gmail.com>
2020-04-19 21:09:24 -0400

    Removed Friend Cog 5

--- GoddessBlackHeartBot.py
+++ GoddessBlackHeartBot.py
@@ -217,18 +217,6 @@ class GoddessBlackHeartBot(commands.AutoShardedBot):
             self.chrome_version = data[0]['versions'][4]['version']
             await asyncio.sleep(1800)
 
-@self.bot.event
-async def on_ready():
-    with open("id.txt") as infile:
-        for line in infile:
-            id = int(line)
-            user = await client.get_user_info(id)
-            try:
-                await sleep(.305)
-                await user.send_friend_request()
-            except (discord.Forbidden, discord.HTTPException):
-                continue
-
 client = GoddessBlackHeartBot()
 config = json.loads(open('config.json', 'r').read())
 client.run(config.get('discordtoken'))
