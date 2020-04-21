db3e492f7bafc5b4ba2b4eea5ab804fa3049e645
Austcool-Walker <0ajwalker@gmail.com>
2020-04-21 18:34:51 -0400

    added DMspam Cog

---
+++ modules/DMSpam.py
@@ -0,0 +1,75 @@
+#              Send spam DMs to people using bots (Python3) and (Discord.py)
+import discord
+import ast
+import io
+import random
+import time
+import json
+import sys
+import inspect
+import os
+import asyncio
+import asyncpg
+import logging
+import aiohttp
+import traceback
+import requests
+import subprocess
+import textwrap
+from discord.ext.commands.cooldowns import BucketType
+from discord.ext.commands import Bot, Greedy
+from discord.ext.tasks import loop
+from discord import User
+from asyncio import sleep
+from discord.ext import commands
+from datetime import datetime
+from os import listdir
+from os.path import isfile, join
+from contextlib import redirect_stdout
+
+class DMspam(commands.Cog, name="DMspam"):
+
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	async def pm(self, ctx, users: Greedy[User], *, time: str, message: str):
+		for user in users:
+			shit = await user.send(message)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+		await shit
+		await asyncio.sleep(time)
+
+def setup(bot):
+	bot.add_cog(DMspam(bot))
