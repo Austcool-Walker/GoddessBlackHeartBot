2f41a475fc00d1dec3437787649e6821065631c9
Austcool-Walker <0ajwalker@gmail.com>
2020-04-22 17:50:33 -0400

    Updated Admin Cog

--- modules/Admin.py
+++ modules/Admin.py
@@ -106,7 +106,7 @@ class Admin(commands.Cog, name="Admin"):
     async def name(self, ctx, name: str):
         '''changes bot global name (BOT OWNER ONLY)'''
         namestr = username=name
-        await self.bot.user.edit('namestr')
+        await self.bot.user.edit(namestr)
         msg = f':ok: change my name: **{name}**'
         await ctx.send(msg)
 
