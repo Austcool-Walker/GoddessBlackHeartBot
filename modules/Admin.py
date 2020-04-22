93770fe95fd7ec12c7d9c1cfeb2e1f1ba05d314b
Austcool-Walker <0ajwalker@gmail.com>
2020-04-22 16:26:34 -0400

    Updated Admin Cog

--- modules/Admin.py
+++ modules/Admin.py
@@ -134,7 +134,7 @@ class Admin(commands.Cog, name="Admin"):
             guild = self.bot.get_guild(int(guildid))
             if guild:
                 await guild.leave()
-        await ctx.send(f":ok_hand: Exit from: {guild.name} ({guild.id}) successful!")
+                await ctx.send(f":ok_hand: Exit from: {guild.name} ({guild.id}) successful!")
         else:
                 msg = ": x: Couldn't find a matching guild for this ID!"
         await ctx.send(msg)
