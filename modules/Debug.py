e7af8d1595597f2884d862ae23caa00939010f8c
Austcool-Walker <0ajwalker@gmail.com>
2020-04-22 21:38:14 -0400

    updated admin Cog

--- modules/Debug.py
+++ modules/Debug.py
@@ -137,11 +137,11 @@ class Debug(commands.Cog, command_attrs=dict(hidden=True), name="Debug"):
             await ctx.send('Downloaded new module ending in {}'.format(len(file)))
 
     @commands.command()
-    async def dl(self, ctx, url, path):
+    async def dl(self, ctx, url, path: str):
         '''Set a new avatar (BOT OWNER ONLY)'''
         if ctx.author.id in AJW_Admins:
-                r = requests.get(f'{1}', stream=True)
-                with open(f'{2}', 'wb') as f:
+                r = requests.get(url, stream=True)
+                with open(path, 'wb') as f:
                         total_length = int(r.headers.get('content-length'))
                         for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
                                 if chunk:
