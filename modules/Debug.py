c5f6ed4fe912eceafce63c66f57ba71dc8161ca0
Austcool-Walker <0ajwalker@gmail.com>
2020-04-23 10:51:32 -0400

    updated admin Cog

--- modules/Debug.py
+++ modules/Debug.py
@@ -137,17 +137,17 @@ class Debug(commands.Cog, command_attrs=dict(hidden=True), name="Debug"):
             await ctx.send('Downloaded new module ending in {}'.format(len(file)))
 
     @commands.command()
-    async def dl(self, ctx, url, path):
+    async def dl(self, ctx, url: str, *path: str):
         '''Downloads File to Hard Drive'''
         if ctx.author.id in AJW_Admins:
-                r = requests.get(url, stream=True)
-                with open(path, 'wb') as f:
+                r = requests.get(int(url, stream=True))
+                with open(int(path, 'wb')) as f:
                         total_length = int(r.headers.get('content-length'))
                         for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
                                 if chunk:
                                     f.write(chunk)
                                     f.flush()
-        await ctx.send(':white_check_mark: downloaded file.')
+        await ctx.send(f':white_check_mark: downloaded file from **{url}** saved **{path}**')
 
     @commands.command()
     @commands.is_owner()
