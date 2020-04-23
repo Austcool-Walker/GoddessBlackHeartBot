ee5f6dda221229d13cdda41f73ab68bcbf82c6d6
Austcool-Walker <0ajwalker@gmail.com>
2020-04-22 20:05:21 -0400

    updated admin Cog

--- modules/Admin.py
+++ modules/Admin.py
@@ -57,8 +57,8 @@ class Admin(commands.Cog, name="Admin"):
                     if chunk:
                         f.write(chunk)
                         f.flush()
-        with open(tempBHFile, 'rb') as f:
-            await self.bot.user.edit(avatar=f.read())
+        await self.bot.user.edit(avatar=f.read())
+        f.close()
         os.remove(tempBHFile)
         asyncio.sleep(2)
         await ctx.send('**:ok:** My new avatar!\n %s' % self.bot.user.avatar_url)
