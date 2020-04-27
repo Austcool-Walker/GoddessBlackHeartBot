29c5337221d3e56f77d5254e0aee24b56b33d6db
Austcool-Walker <0ajwalker@gmail.com>
2020-04-27 17:19:20 -0400

    Updated

--- modules/Moderation.py
+++ modules/Moderation.py
@@ -44,7 +44,7 @@ class Moderation(commands.Cog, name="Moderation"):
         -----------
         :ban 102815825781596160
         '''
-        user = discord.User(userid)
+        user = discord.User(int(userid))
         if user is not None:
             if reason:
                 reason = ' '.join(reason)
