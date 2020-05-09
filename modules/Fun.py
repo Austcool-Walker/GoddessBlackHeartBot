2996b991b8376b68f9d3802945f4d1bbeb163470
Austin Walker <noire@Austins-MacBook-Pro.local>
2020-05-08 20:11:37 -0400

    Updated Fun Cog 5

--- modules/Fun.py
+++ modules/Fun.py
@@ -196,7 +196,7 @@ class Fun(commands.Cog, name="Fun"):
             else:
                 i += 1
         if i == 10 and sys.getsizeof(b) == 88:
-        await self.bot.say("Minecraft Achievement Generator API is bad, pls try again")
+            await self.bot.say("Minecraft Achievement Generator API is bad, pls try again")
         return
         await self.bot.upload(b, filename='achievement.png')
 
