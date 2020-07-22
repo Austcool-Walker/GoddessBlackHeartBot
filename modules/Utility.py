3d729c32b051ede116dceddda920580cf34c4595
Austin Walker <noire@MacBook-Pro.local>
2020-07-22 15:08:06 -0400

    added remindme feature 3

--- modules/Utility.py
+++ modules/Utility.py
@@ -342,7 +342,7 @@ class Utility(commands.Cog, name="Utility"):
 
         : timer 2h stream starts
         '''
-        user = self.bot.get_user(usr)
+        user = self.bot.get_user(int(usr))
         reminder = None
         completed = None
         message = message.replace('@everyone', '@\u200beveryone').replace('@here', '@\u200bhere')
