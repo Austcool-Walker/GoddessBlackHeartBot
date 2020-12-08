c3333a9c4649b04934f324a41148401e96d374b6
Austin Walker <noire@Austins-MacBook-Pro.local>
2020-12-07 21:41:43 -0500

    Updated some stuff

--- GoddessBlackHeartBot.py
+++ GoddessBlackHeartBot.py
@@ -179,7 +179,7 @@ class GoddessBlackHeartBot(commands.AutoShardedBot):
         if self.usedatabase:
             sql = "INSERT INTO guilds (id, name, prefix) VALUES ($1, $2, $3)"
             await self.db.execute(sql, guild.id, guild.name, self.config['prefix'])
-        channel = self.get_channel(768186647958519849)
+        channel = self.get_channel(785695235132358666)
         embed = discord.Embed(title="Guild joined!", color=discord.Colour.blue(),
                               description="We have joined a guild, bringing us to {} guilds!".format(len(self.guilds)))
         embed.add_field(name="Guild name:", value=guild.name)
@@ -192,7 +192,7 @@ class GoddessBlackHeartBot(commands.AutoShardedBot):
         if self.usedatabase:
             sql = "DELETE FROM guilds where id = $1"
             await self.db.execute(sql, guild.id)
-        channel = self.get_channel(768186647958519849)
+        channel = self.get_channel(785695235132358666)
         embed = discord.Embed(title="Guild lost!", color=discord.Colour.red(),
                               description="We have lost a guild, dropping us to {} guilds!".format(len(self.guilds)))
         embed.add_field(name="Guild name:", value=guild.name)
