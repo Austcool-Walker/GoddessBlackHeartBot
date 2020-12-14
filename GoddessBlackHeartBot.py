8bf585ed21f777d153d1d971bdc2140c1963cbe9
Austcool-Walker <61804916+Austcool-Walker@users.noreply.github.com>
2020-12-13 19:53:15 -0500

    removed UTF8 fix

--- GoddessBlackHeartBot.py
+++ GoddessBlackHeartBot.py
@@ -60,7 +60,7 @@ class GoddessBlackHeartBot(commands.AutoShardedBot):
 
         logger = logging.getLogger('discord')
         logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
+        handler = logging.FileHandler(filename='discord.log', mode='a')
         handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
         logger.addHandler(handler)
 
