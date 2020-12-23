e2228a0dfe1c4cf5a0c34a6ef64c4dff1e13cf2e
Austin Walker <noire@Austins-MacBook-Pro.local>
2020-12-23 14:44:55 -0500

    Updated Qoutes

--- modules/Fun.py
+++ modules/Fun.py
@@ -175,7 +175,7 @@ class Fun(commands.Cog, name="Fun"):
 
     @commands.command()
     async def randomquote(self, ctx):
-        quote = self.quotes[str(random.randint(0, 37))]
+        quote = self.quotes[str(random.randint(0, 41))]
 #        await ctx.send(quote.encode("utf-8"))
         await ctx.send(quote)
 
